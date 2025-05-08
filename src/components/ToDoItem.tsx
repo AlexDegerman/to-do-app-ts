8a8ada3483e0f2bd2363e51a2c9130f25965e0a9
AlexDegerman <alex.degerman.dev@gmail.com>
2025-05-09 00:26:40 +0300

    Add creation date to tasks

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -8,6 +8,13 @@ interface ToDoItemProps {
 }
 
 const ToDoItem = ({ task, toggleComplete, deleteTask }: ToDoItemProps) => {
+  const formatDate = (timestamp: number) => {
+    return new Date(timestamp).toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    })
+  }
 
   return (
     <div>
@@ -16,6 +23,7 @@ const ToDoItem = ({ task, toggleComplete, deleteTask }: ToDoItemProps) => {
       onChange={() => toggleComplete(task.id)}
       />
       <span>{task.text}</span>
+      <span>{formatDate(task.id)}</span>
       <button onClick={() => deleteTask(task.id)}><Trash2Icon/></button>
     </div>
   )
