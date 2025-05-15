d13d52651da9a8936a1e5179fa575a88addc9ef7
AlexDegerman <alex.degerman.dev@gmail.com>
2025-05-15 03:34:58 +0300

    fix deploy

--- src/store/todoSlice.ts
+++ src/store/todoSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, type PayloadAction, createSelector } from '@reduxjs/toolkit'
-import { type RootState } from './index'
+import { type RootState } from './types'
 
 export interface Task {
   id: number
